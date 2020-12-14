312f4d353d254f40112749d1789a04a5093f746a
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-14 12:30:24 +0100

    setup functions for open/close buttons

--- script.js
+++ script.js
@@ -10,15 +10,37 @@ function Book(title, author, pages, read) {
   }
 }
 
-const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295, true)
-console.log(book1.info())
 
-myLibrary.push(book1)
-
-for (let i=0; i<myLibrary.length; i++) {
-  
-}
 
 function addBookToLibrary() {
   // do stuff here
+
+  const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295, true)
+  console.log(book1.info())
+
+  myLibrary.push(book1)
+  const books = document.querySelector('div');
+
+  for (let i=0; i<myLibrary.length; i++) {
+    const para = document.createElement('p');
+    para.textContent = myLibrary[i].info();
+    books.appendChild(para);
+  }
 }
+
+addBookToLibrary()
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+const openFormBtn = document.getElementById("addBook")
+openFormBtn.onclick = openForm
+
+const closeFormBtn = document.getElementById("close")
+closeFormBtn.onclick = closeForm
