2b6e37e55a52765582e9e30066ba12668a5a964f
Moses Akor <gabrielmosesakor@gmail.com>
2020-12-15 12:59:00 +0100

    fixed an issue where deleteing books would somtimes delete the wrong book from library

--- script.js
+++ script.js
@@ -10,16 +10,55 @@ function Book(title, author, pages, read) {
   }
 }
 
+function addBookToLibrary() {
+  let userInput = getUserInput();
+  if (isRead() == true) userInput[userInput.length-1] = true
+  const book = new Book(...userInput);
+  myLibrary.push(book);
+  resetForm();
+  displayBooks();
+  closeForm();
+}
 
 function getUserInput() {
   return Object.values(Array.from(document.querySelectorAll('input')).reduce((a, input) => ({...a, [input.id]: input.value}), {}));
 }
 
-function addBookToLibrary() {
-  const userInput = getUserInput();
-  const book = new Book(...userInput);
-  myLibrary.push(book);
+function isRead() {
+  return document.getElementById('read').checked
 }
+
+function displayBooks() {
+  const booksContainer = document.getElementById('booksContainer');
+  const books = document.querySelectorAll('li');
+  books.forEach(book => booksContainer.removeChild(book));
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    addBookToPage(myLibrary[i])
+  }
+}
+
+function addBookToPage(book) {
+    const bookIndex = myLibrary.indexOf(book);
+    const list = document.querySelector('ul');
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = book.info();
+    listItem.dataset.id = bookIndex;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e) {
+      myLibrary.splice(bookIndex, 1)
+      list.removeChild(listItem);
+      displayBooks();
+    }
+  }
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
@@ -27,6 +66,10 @@ function openForm() {
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+
+function resetForm() {
+  document.getElementById("form").reset()
 }
 
 const openFormBtn = document.getElementById("addBook");
